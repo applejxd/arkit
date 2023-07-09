c7bf6702e401da94c2bcf6f59adc33617dbfe24e
applejxd <9639592+applejxd@users.noreply.github.com>
2023-07-09 15:18:05 +0900

    Initial Commit

---
+++ arkit-experimentUITests/arkit_experimentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  arkit_experimentUITestsLaunchTests.swift
+//  arkit-experimentUITests
+//
+//  Created by Masashi Yamazaki on 2023/07/09.
+//
+
+import XCTest
+
+final class arkit_experimentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
